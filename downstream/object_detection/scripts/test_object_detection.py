39a2af622ddacd43c5addd8fbcf272dc8d464cc5
Karun <karunpr1@uni-bremen.de>
2024-12-08 20:18:24 +0100

    [DEV-40] Added mlflow to log test metrics

--- downstream/object_detection/scripts/test_object_detection.py
+++ downstream/object_detection/scripts/test_object_detection.py
@@ -2,14 +2,17 @@ from detectron2.engine import DefaultPredictor
 from .detectron_utils import *
 
 
-def do_coco_eval(config_file, detectron_output_dir):
+def eval_model(config_file, detectron_output_dir, test_dataset_name):
     cfg = get_test_cfg(config_file)
     cfg.defrost()
     cfg.OUTPUT_DIR = detectron_output_dir
     cfg.MODEL.WEIGHTS = os.path.join(detectron_output_dir, "model_final.pth")
     cfg.freeze()
-    test_dataset = cfg.DATASETS.TEST[0]
-    logger.info(f"Using {test_dataset} dataset for evaluating the final model.")
+    logger.info(f"Using {test_dataset_name} dataset for evaluating the final model.")
     predictor = DefaultPredictor(cfg)
-    coco_evaluator(cfg, predictor, test_dataset)
-    test_image(test_dataset, predictor, n=5, threshold=0.5)
+    evaluation_results = coco_evaluator(cfg, predictor, test_dataset_name)
+    for k, v in evaluation_results["bbox"].items():
+        mlflow.log_metric(f"Test Set {k}", v, step=0)
+    mlflow.log_artifacts(detectron_output_dir, "test-set-evaluation")
+    mlflow.log_text(str(evaluation_results), "test-set-evaluation/coco-metrics.txt")
+    # test_image(test_dataset_name, predictor, n=5, threshold=0.5)
