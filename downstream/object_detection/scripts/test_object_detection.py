bb7c04d1b6ab80f1541e5aad6747bb9c423328f9
Karun <karun.parimalam@ubica-robotics.eu>
2025-02-10 15:23:39 +0100

    [DEV-40] Disabled mlflow artifact logging

--- downstream/object_detection/scripts/test_object_detection.py
+++ downstream/object_detection/scripts/test_object_detection.py
@@ -13,6 +13,6 @@ def eval_model(config_file, detectron_output_dir, test_dataset_name):
     evaluation_results = coco_evaluator(cfg, predictor, test_dataset_name)
     for k, v in evaluation_results["bbox"].items():
         mlflow.log_metric(f"Test Set {k}", v, step=0)
-    mlflow.log_artifacts(detectron_output_dir, "test-set-evaluation")
+    # mlflow.log_artifacts(detectron_output_dir, "test-set-evaluation")
     mlflow.log_text(str(evaluation_results), "test-set-evaluation/coco-metrics.txt")
     # test_image(test_dataset_name, predictor, n=5, threshold=0.5)
