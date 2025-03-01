9fef65cef15d86bae2dc8dd49113ae12d5792e7b
Karun <karunpr1@uni-bremen.de>
2025-03-01 22:09:26 +0100

    [DEV-49] add evaluate param keys for hydra config

--- downstream/object_detection/config.py
+++ downstream/object_detection/config.py
@@ -62,6 +62,14 @@ class Mlfow:
     run_name: str
     run_description: str
 
+@dataclass
+class Evaluate:
+    test_dataset_name: str
+    test_images: str
+    test_annotations: str
+    config_file_path: str
+    model_output_dir: str
+    model_name: str
 
 @dataclass
 class DetectronConfig:
@@ -71,3 +79,4 @@ class DetectronConfig:
     model: Model
     solver: Solver
     mlflow: Mlfow
+    evaluate: Evaluate
