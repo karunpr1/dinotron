f454df60fc6caa93f10ca48623236174d82d4e3f
Karun <karunpr1@uni-bremen.de>
2025-06-05 13:29:08 +0200

    [DEV-49] add options to modify detectron config for custom trainer

--- downstream/object_detection/scripts/train_object_detection.py
+++ downstream/object_detection/scripts/train_object_detection.py
@@ -9,6 +9,13 @@ def train_with_default_trainer(cfg, resume=True):
 
 
 def train_with_custom_trainer(cfg, resume=True):
+    # cfg.defrost()
+    # cfg.SOLVER.OPTIMIZER = "AdamW" # SGD
+    # cfg.SOLVER.WEIGHT_DECAY = 0.01
+    # cfg.SOLVER.AMSGRAD = False
+    # cfg.SOLVER.WARMUP_FACTOR = 0.001
+    # cfg.SOLVER.WARMUP_METHOD = "linear"
+    # cfg.freeze()
     trainer = CustomTrainer(cfg)
     trainer.register_hooks([MLflowHook(cfg)])
     trainer.resume_or_load(resume=resume)
